f803ce09b5d593c95d6ae1bfcde68ed390f64c3d
Tushar Tambe <44019295+tushartambe@users.noreply.github.com>
2020-03-18 15:25:12 +0530

    Implement draw results

--- src/mastermind/core.cljs
+++ src/mastermind/core.cljs
@@ -2,12 +2,13 @@
   (:require
     [reagent.core :as r]))
 (def colors ["red" "green" "purple" "maroon" "blue" "darkorange" "darkgoldenrod" "black"])
+(def results {:r "red" :w "white"})
 (defonce game (r/atom {
-                       :code         [1 2 3 4 5]
-                       :current-turn 0
-                       :attempts     (into [] (repeat 12 {:guess [0 5 3 2 4] :result []}))
+                       :code           [1 2 3 4 5]
+                       :current-turn   0
+                       :attempts       (into [] (repeat 12 {:guess [0 5 3 2 4] :result [:r :w :r :e :w]}))
                        :selected-color 2
-                       :won?         false
+                       :won?           false
                        }))
 
 (defn draw-color [index attempt]
@@ -20,6 +21,7 @@
          :class ["color" "clickable" (when (= index (:selected-color @game)) "selected")]
          :key   index}]
   )
+
 (defn draw-empty [index]
   [:div {:style {:background (str "radial-gradient(circle at 60% 20%, white 1px, gray 60%)")}
          :class "color"
@@ -31,8 +33,21 @@
    (if (empty? attempt) (map-indexed draw-empty (:code @game)) (map-indexed draw-color attempt))
    ])
 
+(defn draw-empty-attempt-result [index]
+  [:div {:class "result"
+         :key   index}
+   ]
+  )
+
+(defn draw-attempt-result-colors [index result]
+  [:div {:style {:background-color (results result)}
+         :class "result"
+         :key   index}
+   ])
+
 (defn draw-attempt-result [result]
   [:div {:class "attempt-result"}
+   (if (empty? result) (map-indexed draw-empty-attempt-result (:code @game)) (map-indexed draw-attempt-result-colors result))
    ])
 
 (defn draw-board [turn attempt]
