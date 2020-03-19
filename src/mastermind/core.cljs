ea841d2a0cfcd72cf3d99cec2418e6d7f829e2c4
anupingale <annu262000@gmail.com>
2020-03-19 16:29:21 +0530

    Added validation to display result of guess.

--- src/mastermind/core.cljs
+++ src/mastermind/core.cljs
@@ -1,13 +1,14 @@
 (ns mastermind.core
   (:require
-    [reagent.core :as r]))
+    [reagent.core :as r]
+    [mastermind.logic :as l]))
 (def colors ["red" "green" "purple" "maroon" "blue" "darkorange" "darkgoldenrod" "black" "gray"])
 (def results {:r "red" :w "white"})
 (defonce game (r/atom {
                        :code           [1 2 3 4 5]
                        :current-turn   0
                        :attempts       (into [] (repeat 12 {:guess [8 8 8 8 8] :result []}))
-                       :selected-color 0
+                       :selected-color 1
                        :won?           false
                        }))
 
@@ -24,9 +25,10 @@
            :onClick (when is-clickable (partial set-color turn index))}]))
 
 (defn draw-color-palatte [index color]
-  [:div {:style {:background (str "radial-gradient(circle at 65% 15%, white 1px, " color " 60%)")}
-         :class ["color" "clickable" (when (= index (:selected-color @game)) "selected")]
-         :key   index}])
+  [:div {:style   {:background (str "radial-gradient(circle at 65% 15%, white 1px, " color " 60%)")}
+         :class   ["color" "clickable" (when (= index (:selected-color @game)) "selected")]
+         :onClick (fn [] (swap! game assoc :selected-color index))
+         :key     index}])
 
 (defn draw-attempt [attempt turn]
   [:div {:class "attempt"}
@@ -49,6 +51,11 @@
    (if (empty? result) (map-indexed draw-empty-attempt-result (:code @game)) (map-indexed draw-attempt-result-colors result))
    ])
 
+(defn validate-guess []
+  (swap! game assoc :attempts (update-in (:attempts @game) [(:current-turn @game) :result] (partial l/get-result (:code @game) (:guess ((:attempts @game) (:current-turn @game))))))
+  (swap! game assoc :current-turn (inc (:current-turn @game)))
+  )
+
 (defn draw-board [turn attempt]
   [:div {:class "guess" :key turn}
    (draw-attempt (:guess attempt) turn)
@@ -56,19 +63,21 @@
    ])
 
 (defn home-page []
-  [:div {:class "container"}
-   [:header {:class "header"} "Mastermind"]
-   [:div {:class "board"}
-    [:div {:class "secret"}
-     [:div {:class "code"}]
-     [:input {:type "Button" :class "submit-btn"}]
-     ]
-    [:div {:class "guesses"}
-     (map-indexed draw-board (:attempts @game))]
-    ]
-   [:div {:class "color-palette"}
-    (map-indexed draw-color-palatte colors)
-    ]])
+  (let [is-code-complete (every? (partial not= 8) (:guess ((:attempts @game) (:current-turn @game))))]
+    [:div {:class "container"}
+     [:header {:class "header"} "Mastermind"]
+     [:div {:class "board"}
+      [:div {:class "secret"}
+       [:div {:class "code"}]
+       [:button {:class   ["submit-btn" (when-not is-code-complete "disable-btn")]
+                 :onClick (when is-code-complete validate-guess)
+                 } "submit"]]
+      [:div {:class "guesses"}
+       (map-indexed draw-board (:attempts @game))]
+      ]
+     [:div {:class "color-palette"}
+      (map-indexed draw-color-palatte colors)
+      ]]))
 
 ;; -------------------------
 ;; Initialize app
--- src/mastermind/logic.cljs
+++ src/mastermind/logic.cljs
@@ -5,12 +5,11 @@
 (defn generate-code []
   (take 5 (shuffle colors)))
 
-(defn get-reds [c p]
-  (count (filter true? (map #(= %1 %2) c p))))
+(defn get-reds [code pattern]
+  (count (filter true? (map #(= %1 %2) code pattern))))
 
-(defn get-whites [c p]
-  (- (count (filter true? (map #(some (partial = %1) c) (set p)))) (get-reds c p)))
+(defn get-whites [code pattern]
+  (- (count (filter true? (map #(some (partial = %1) code) (set pattern)))) (get-reds code pattern)))
 
-(defn get-result [c p]
-  {:r (get-reds c p)
-   :w (get-whites c p)})
+(defn get-result [code pattern]
+  (into [] (take 5 (concat (repeat (get-reds code pattern) :r) (repeat (get-whites code pattern) :w) (repeat 5 :other)))))
